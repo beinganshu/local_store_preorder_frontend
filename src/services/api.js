aa10f31ed54418b8acf1d55b25eec65f5e484e1e
Anshuman Sahoo <122696238+beinganshu@users.noreply.github.com>
2025-09-04 09:59:26 +0530

    Change API base URL to production endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://anxhu2004-local-store-backend.hf.space",
 });
 
 export default API;
+
